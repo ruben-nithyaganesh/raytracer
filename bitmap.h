0a93a5e6123847ffd50a898835830d52dfd8ad75
Ruben <66393567+ruben-nithyaganesh@users.noreply.github.com>
2025-05-14 16:45:27 +1200

    comment

--- bitmap.h
+++ bitmap.h
@@ -9,87 +9,87 @@ typedef struct Pixel {
     unsigned char r, g, b;
 } Pixel;
 
-void save_bitmap(Pixel data[], const char* file_name, int width, int height, int dpi) {
+// void save_bitmap(Pixel data[], const char* file_name, int width, int height, int dpi) {
 
-    struct bitmap_file_header {
-        unsigned char   bitmap_type[2];     // 2 bytes
-        int             file_size;          // 4 bytes
-        short           reserved1;          // 2 bytes
-        short           reserved2;          // 2 bytes
-        unsigned int    offset_bits;        // 4 bytes
-    } bfh;
+//     struct bitmap_file_header {
+//         unsigned char   bitmap_type[2];     // 2 bytes
+//         int             file_size;          // 4 bytes
+//         short           reserved1;          // 2 bytes
+//         short           reserved2;          // 2 bytes
+//         unsigned int    offset_bits;        // 4 bytes
+//     } bfh;
 
-    // bitmap image header (40 bytes)
-    struct bitmap_image_header {
-        unsigned int    size_header;        // 4 bytes
-        unsigned int    width;              // 4 bytes
-        unsigned int    height;             // 4 bytes
-        short int       planes;             // 2 bytes
-        short int       bit_count;          // 2 bytes
-        unsigned int    compression;        // 4 bytes
-        unsigned int    image_size;         // 4 bytes
-        unsigned int    ppm_x;              // 4 bytes
-        unsigned int    ppm_y;              // 4 bytes
-        unsigned int    clr_used;           // 4 bytes
-        unsigned int    clr_important;      // 4 bytes
-    } bih;
+//     // bitmap image header (40 bytes)
+//     struct bitmap_image_header {
+//         unsigned int    size_header;        // 4 bytes
+//         unsigned int    width;              // 4 bytes
+//         unsigned int    height;             // 4 bytes
+//         short int       planes;             // 2 bytes
+//         short int       bit_count;          // 2 bytes
+//         unsigned int    compression;        // 4 bytes
+//         unsigned int    image_size;         // 4 bytes
+//         unsigned int    ppm_x;              // 4 bytes
+//         unsigned int    ppm_y;              // 4 bytes
+//         unsigned int    clr_used;           // 4 bytes
+//         unsigned int    clr_important;      // 4 bytes
+//     } bih;
 
-    // create a file object that we will use to write our image
-    FILE *image;
-    // we want to know how many pixels to reserve
-    int image_size = width * height;
-    // a byte is 4 bits but we are creating a 24 bit image so we can represent a pixel with 3
-    // our final file size of our image is the width * height * 4 + size of bitmap header
-    int file_size = 54 + 4 * image_size;
-    // pixels per meter https://www.wikiwand.com/en/Dots_per_inch
-    int ppm = dpi * 39.375;
+//     // create a file object that we will use to write our image
+//     FILE *image;
+//     // we want to know how many pixels to reserve
+//     int image_size = width * height;
+//     // a byte is 4 bits but we are creating a 24 bit image so we can represent a pixel with 3
+//     // our final file size of our image is the width * height * 4 + size of bitmap header
+//     int file_size = 54 + 4 * image_size;
+//     // pixels per meter https://www.wikiwand.com/en/Dots_per_inch
+//     int ppm = dpi * 39.375;
 
-    // if you are on Windows you can include <windows.h>
-    // and make use of the BITMAPFILEHEADER and BITMAPINFOHEADER structs
+//     // if you are on Windows you can include <windows.h>
+//     // and make use of the BITMAPFILEHEADER and BITMAPINFOHEADER structs
 
-    memcpy(&bfh.bitmap_type, "BM", 2);
-    bfh.file_size       = file_size;
-    bfh.reserved1       = 0;
-    bfh.reserved2       = 0;
-    bfh.offset_bits     = 0;
+//     memcpy(&bfh.bitmap_type, "BM", 2);
+//     bfh.file_size       = file_size;
+//     bfh.reserved1       = 0;
+//     bfh.reserved2       = 0;
+//     bfh.offset_bits     = 0;
 
-    bih.size_header     = sizeof(bih);
-    bih.width           = width;
-    bih.height          = height;
-    bih.planes          = 1;
-    bih.bit_count       = 24;
-    bih.compression     = 0;
-    bih.image_size      = image_size;
-    bih.ppm_x           = ppm;
-    bih.ppm_y           = ppm;
-    bih.clr_used        = 0;
-    bih.clr_important   = 0;
+//     bih.size_header     = sizeof(bih);
+//     bih.width           = width;
+//     bih.height          = height;
+//     bih.planes          = 1;
+//     bih.bit_count       = 24;
+//     bih.compression     = 0;
+//     bih.image_size      = image_size;
+//     bih.ppm_x           = ppm;
+//     bih.ppm_y           = ppm;
+//     bih.clr_used        = 0;
+//     bih.clr_important   = 0;
 
-    image = fopen(file_name, "wb");
+//     image = fopen(file_name, "wb");
 
-    // compiler woes so we will just use the constant 14 we know we have
-    fwrite(&bfh, 1, 14, image);
-    fwrite(&bih, 1, sizeof(bih), image);
+//     // compiler woes so we will just use the constant 14 we know we have
+//     fwrite(&bfh, 1, 14, image);
+//     fwrite(&bih, 1, sizeof(bih), image);
 
-    // write out pixel data, one last important this to know is the ordering is backwards
-    // we have to go BGR as opposed to RGB
-    for (int i = 0; i < image_size; i++) {
-        Pixel BGR = data[i];
+//     // write out pixel data, one last important this to know is the ordering is backwards
+//     // we have to go BGR as opposed to RGB
+//     for (int i = 0; i < image_size; i++) {
+//         Pixel BGR = data[i];
 
-        float red   = (BGR.r);
-        float green = (BGR.g);
-        float blue  = (BGR.b);
+//         float red   = (BGR.r);
+//         float green = (BGR.g);
+//         float blue  = (BGR.b);
 
-        // if you don't follow BGR image will be flipped!
-        unsigned char color[3] = {
-            blue, green, red
-        };
+//         // if you don't follow BGR image will be flipped!
+//         unsigned char color[3] = {
+//             blue, green, red
+//         };
 
-        fwrite(color, 1, sizeof(color), image);
-    }
+//         fwrite(color, 1, sizeof(color), image);
+//     }
 
-    fclose(image);
-}
+//     fclose(image);
+// }
 
 void save_ppm(const char* filename, Pixel data[], int width, int height) {
     FILE *file = fopen(filename, "wb");
